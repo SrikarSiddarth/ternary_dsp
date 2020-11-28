7f2282f0e65dbbe5acef36c6fd2c55c4ea7854a5
SrikarSiddarth <54308546+SrikarSiddarth@users.noreply.github.com>
2020-11-28 12:41:24 +0530

    Add files via upload

---
+++ trigger.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Empty
+import sys, select, os
+if os.name=='nt':
+	import msvcrt
+else:
+	import tty, termios
+
+
+msg = """
+	
+	Welcome to the ternary pulse compression code simulation.
+	Trigger the code by pressing the 't' key on the keyboard!
+
+
+Ctrl-c to quit
+"""
+e = """
+Communications failed
+"""
+
+def getKey():
+	if os.name=='nt':
+		return msvcrt.getch()
+
+	tty.setraw(sys.stdin.fileno())
+	rlist,_,_ = select.select([sys.stdin],[],[],0.1)
+	if rlist:
+		key = sys.stdin.read(1)
+	else:
+		key = ''
+
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+	return key
+
+
+if __name__ == '__main__':
+	if os.name!='nt':
+		settings = termios.tcgetattr(sys.stdin)
+
+	rospy.init_node('trigger_teleop')
+	pub = rospy.Publisher('/trigger', Empty, queue_size=10)
+
+	try:
+		print(msg)
+		while(1):
+			key = getKey()
+			if key=='t':
+				pub.publish()
+				print('triggered!')
+			elif key=='\x03':
+				break
+	except:
+		print(e)
+
+	if os.name!='nt':
+		termios.tcsetattr(sys.stdin,termios.TCSADRAIN, settings)
